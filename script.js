cdaeef02a7d141daf26299af594c4e5c5403da3e
stephanietavar <119288636+stephanietavar@users.noreply.github.com>
2022-11-28 05:28:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function abrirMenu(){
+    let barraMenu = document.getElementById('barra-menu');
+
+    if(barraMenu.indexstyele.width == "0px"){
+        barraMenu.indexstyele.width = "180px";
+    }else{
+        barraMenu.indexstyele.width = "0px"
+    }
+}
